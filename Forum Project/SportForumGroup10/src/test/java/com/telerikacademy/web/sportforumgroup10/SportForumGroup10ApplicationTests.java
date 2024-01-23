d0d1e31c4f5ff7f8924a08210aeafdfa9fff6c54
dinkovachev <147801675+dinkovachev@users.noreply.github.com>
2024-01-23 12:14:42 +0200

    First upload
    Innit commit

---
+++ Forum Project/SportForumGroup10/src/test/java/com/telerikacademy/web/sportforumgroup10/SportForumGroup10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telerikacademy.web.sportforumgroup10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportForumGroup10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Forum Project/db/create.sql
@@ -0,0 +1,64 @@
+create table users
+(
+    user_id    int auto_increment
+        primary key,
+    first_name varchar(32) not null,
+    last_name  varchar(32) not null,
+    email      varchar(50) not null,
+    username   varchar(20) not null,
+    password   varchar(20) not null,
+    is_blocked tinyint(1)  not null,
+    constraint email
+        unique (email),
+    constraint username
+        unique (username)
+);
+
+create table admins
+(
+    admin_id     int auto_increment
+        primary key,
+    user_id      int         not null,
+    phone_number varchar(20) null,
+    constraint user_id
+        foreign key (user_id) references users (user_id)
+);
+
+create table posts
+(
+    post_id         int auto_increment
+        primary key,
+    title           varchar(64)   not null,
+    content         varchar(8192) not null,
+    comment_id      int           not null,
+    number_of_likes int           null,
+    author_id       int           not null,
+    constraint author_id
+        foreign key (author_id) references users (user_id)
+);
+
+create table comments
+(
+    comment_id int           not null
+        primary key,
+    content    varchar(8192) not null,
+    author_id  int           not null,
+    post_id    int           not null,
+    constraint comment_author_id
+        foreign key (author_id) references users (user_id),
+    constraint post_id
+        foreign key (post_id) references posts (post_id)
+);
+
+create table likes
+(
+    like_id       int auto_increment
+        primary key,
+    liked_post_id int not null,
+    author_id     int not null,
+    constraint liked_post_id
+        foreign key (liked_post_id) references posts (post_id),
+    constraint user_liked_id
+        foreign key (author_id) references users (user_id)
+);
+
