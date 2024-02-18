dc1d33c545b01faee959883181372a2935944ebb
vstZstoyanov <ventsislav.stoyanov.a56@learn.telericakademy.com>
2024-02-18 13:27:52 +0200

    create methods in PostMvcController

--- Forum Project/SportForumGroup10/src/main/java/com/telerikacademy/web/sportforumgroup10/helpers/PostMapper.java
+++ Forum Project/SportForumGroup10/src/main/java/com/telerikacademy/web/sportforumgroup10/helpers/PostMapper.java
@@ -3,12 +3,23 @@ package com.telerikacademy.web.sportforumgroup10.helpers;
 import com.telerikacademy.web.sportforumgroup10.models.Dto.PostDto;
 import com.telerikacademy.web.sportforumgroup10.models.Post;
 import com.telerikacademy.web.sportforumgroup10.models.User;
+import com.telerikacademy.web.sportforumgroup10.services.Contracts.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
 @Component
 public class PostMapper {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostMapper(PostService postService) {
+        this.postService= postService;
+    }
+
+
      public Post fromDtoIn(PostDto postDto, User creator) {
          Post post = new Post();
          post.setTitle(postDto.getTitle());
@@ -18,4 +29,20 @@ public class PostMapper {
          return post;
 
      }
+    public Post fromDto(PostDto dto) {
+        Post post = new Post();
+
+        post.setTitle(dto.getTitle());
+        post.setContent(dto.getContent());
+
+        return post;
+    }
+
+    public PostDto toDto(Post post) {
+        PostDto dto = new PostDto();
+        dto.setTitle(post.getTitle());
+        dto.setContent(post.getContent());
+        return dto;
+    }
 }
+
